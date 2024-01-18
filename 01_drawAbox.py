5c87a9abb2d46a091462252c3da5ce056ddcafac
Sia Jam <47018483+3ia@users.noreply.github.com>
2024-01-18 07:22:04 -0800

    Add files via upload

---
+++ 01_drawAbox.py
@@ -0,0 +1,21 @@
+
+
+# importing image object from PIL
+import random
+from PIL import Image, ImageDraw
+
+#w, h = 220, 190
+#shape = [(40, 40), (w - 10, h - 10)]
+
+# creating new Image object
+for i in range(10):
+    
+    print(i)
+    img = Image.new("RGB", (500, 500), (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)))
+
+# create line image
+#img1 = ImageDraw.Draw(img)
+#img1.line(shape, fill ="red", width = 0)
+
+
+    img.save('boxes/'+str(i)+'500'+'.jpg')
